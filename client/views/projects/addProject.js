f5ab44623b86fc9dc748d33202757f79ffb43ec7
Dev Labs <devlabs1980@gmail.com>
2015-10-12 17:02:53 -0700

    Add Project Form with schema

---
+++ client/views/projects/addProject.js
@@ -0,0 +1,10 @@
+Template.addProject.helpers({
+	EpicOptions: function() {
+    return [
+      {label: "Security", value: "Security"},
+      {label: "Mobile Computing", value:  "Mobile Computing"},
+      {label: "Digital Workspace", value: "Digital Workspace"}
+    ]
+    }
+	
+});
