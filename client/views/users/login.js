10f7d14647bfbc54a07d68e941db867d7d1b5db0
Dev Labs <devlabs1980@gmail.com>
2015-10-14 14:21:37 -0700

    Fixed Admin, Changed Logo

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -1,15 +1,3 @@
-Template.login.events = {
-  'click input[type=submit]': function(event){
-    event.preventDefault();
-    var username = $('#username').val();
-    var password = $('#password').val();
-    Meteor.loginWithPassword(username, password, function(error){
-      if(error){
-        flash(error.reason, 'error');
-      }else{
-        Router.go('/');
-        flash('You are now logged in.');
-      }
-    });
-  }
-};
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
--- server/seed.js
+++ server/seed.js
@@ -7,3 +7,34 @@ Fill in the app with dummy data if database is empty.
 /+ ---------------------------------------------------- */
 
 // Fixture data 
+
+
+var users = [
+     /* {name:"Normal User",email:"normal@example.com",roles:[]},
+      {name:"View-Secrets User",email:"view@example.com",roles:['view-secrets']},
+      {name:"Manage-Users User",email:"manage@example.com",roles:['manage-users']}, */
+      {name:"Admin User",email:"tmb@cgelsie.com",roles:['admin']}
+    ];
+
+_.each(users, function (user) {
+  var id;
+  console.log(user.email);
+
+  if (!Accounts.findUserByUsername('tmb')) {
+
+  id = Accounts.createUser({
+    email: user.email,
+    username: 'tmb',
+    password: "tmb123",
+    profile: { name: user.name }
+  });
+
+  if (user.roles.length > 0) {
+    // Need _id of existing user record so this call must come 
+    // after `Accounts.createUser` or `Accounts.onCreate`
+    Roles.addUsersToRoles(id, user.roles);
+  }
+  }
+  else {console.log('Found a user already');}
+});
+
