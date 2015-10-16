8cb810efe03ca86f6b2922c6148fd0f425ee1f8a
Dev Labs <devlabs1980@gmail.com>
2015-10-16 08:39:30 -0700

    Updated overview navigation to projects from app btn

--- client/main.js
+++ client/main.js
@@ -7,11 +7,11 @@ Global client-side code. Loads last.
 /+ ---------------------------------------------------- */
 
 //
-/*
+
 jQuery.ajax({
             url: "https://jira.capgroup.com/s/1062dd9ae25fe20faf360140528ee1db-T/en_USkahc8z/6346/6/1.4.16/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=3324184f",
         type: "get",
     cache: true,
     dataType: "script"
 });
-*/
+
