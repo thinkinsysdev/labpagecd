38a72eeb465521f7c3957e1342ed851a4582aa40
Dev Labs <devlabs1980@gmail.com>
2015-10-16 08:48:27 -0700

    Removed items publications

--- server/publications.js
+++ server/publications.js
@@ -8,16 +8,6 @@ All publications-related code.
 
 // Publish all items
 
-Meteor.publish('allItems', function() {
-  return Items.find();
-});
-
-// Publish a single item
-
-Meteor.publish('singleItem', function(id) {
-  return Items.find(id);
-});
-
 Meteor.publish('allProjects', function() {
   return Projects.find();
 });
