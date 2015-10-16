91ccdd72bb99d26458549526a0fc821e5fce8a1e
Dev Labs <devlabs1980@gmail.com>
2015-10-16 05:10:02 -0700

    Added logins and the blockquote

--- server/seed.js
+++ server/seed.js
@@ -9,23 +9,30 @@ Fill in the app with dummy data if database is empty.
 // Fixture data 
 
 
-var users = [
+var users = [{name:"tmb", email:"tmb@capgroup.com", roles:['admin']},
+      {name:"bcy", email:"bcy@capgroup.com", roles:['admin']},
+      {name:"jtfl", email:"jtfl@capgroup.com", roles:['admin']},
+      {name:"jbxr", email:"jbxr@capgroup.com", roles:['admin']},
+      {name:"axxs", email:"axxs@capgroup.com", roles:['admin']},
+      {name:"maum", email:"maum@capgroup.com", roles:['admin']}
      /* {name:"Normal User",email:"normal@example.com",roles:[]},
       {name:"View-Secrets User",email:"view@example.com",roles:['view-secrets']},
       {name:"Manage-Users User",email:"manage@example.com",roles:['manage-users']}, */
-      {name:"Admin User",email:"tmb@cgelsie.com",roles:['admin']}
+    //  {name:"Admin User",email:"tmb@cgelsie.com",roles:['admin']}
     ];
 
 _.each(users, function (user) {
   var id;
   console.log(user.email);
 
-  if (!Accounts.findUserByUsername('tmb')) {
+  var foundUser = Accounts.findUserByUsername(user.name);
+
+  if (!foundUser) {
 
   id = Accounts.createUser({
     email: user.email,
-    username: 'tmb',
-    password: "tmb123",
+    username: user.name,
+    password: user.name + '123',
     profile: { name: user.name }
   });
 
@@ -33,8 +40,18 @@ _.each(users, function (user) {
     // Need _id of existing user record so this call must come 
     // after `Accounts.createUser` or `Accounts.onCreate`
     Roles.addUsersToRoles(id, user.roles);
+
+
   }
+
+  //houston_admins.insert({"user_id":id});
+
+
   }
-  else {console.log('Found a user already');}
+  else {console.log('Found a user already ')+foundUser;}
+
+
+
 });
 
+//console.log(houston_admins.find().count() + 'admins created');
