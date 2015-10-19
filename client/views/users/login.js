f2924193e8452366fa67c5dd7e829ef87239f150
Dev Labs <devlabs1980@gmail.com>
2015-10-18 22:05:25 -0700

    Added simple back up package

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -1,3 +1,18 @@
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
+
+Template.login.helpers({
+	 isAdmin: function() {
+    	var currentUser = Meteor.user();
+
+      console.log(Roles.userIsInRole(currentUser, ['admin']) + ' : user is in role');
+    // Is this hackable?
+     if (Roles.userIsInRole(currentUser, ['admin']))
+    {
+        return true;
+    }
+
+    }
+
+})
