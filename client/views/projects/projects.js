2934e98abcbd84095b9ab6be1eda73fc4622e401
Dev Labs <devlabs1980@gmail.com>
2015-10-20 22:51:33 -0700

    Added logic for tags on the labPhase tag

--- client/views/projects/projects.js
+++ client/views/projects/projects.js
@@ -5,6 +5,65 @@ Template.projects.created = function () {
 
 Template.projects.helpers({
   //
+  convertPhasetoIcon: function(strPhase) {
+  	console.log('This conversion was succcessful ' + strPhase)
+  	
+
+  	switch(strPhase) {
+ 	
+ 	case "Idea": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+							+ '<a class="btn btn-success"><i class="fa fa-lightbulb-o"></i></a>'
+							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+						+' </div>';
+				  break;
+	case "Hypothesis": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
+							+ ' <a class="btn btn-success"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+						+' </div>';
+						break;
+	case "Experimentation": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
+							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn btn-success"><i class="fa fa-flask"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+						+' </div>';
+						break;
+	case "Demo": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
+							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn "><i class="fa fa-flask"></i> </a>'
+							+ '<a class="btn btn-success"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+						+' </div>';
+						break;
+	case "Graduate": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
+							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn btn-success"><i class="fa fa-graduation-cap"></i> </a>'
+						+' </div>';
+						break;
+	default: strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
+							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+						+' </div>';
+						break;
+	}
+
+	return strReturn;
+
+  }
 
 });
 
