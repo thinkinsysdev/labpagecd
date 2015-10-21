57d8fe5ab9bfd4808eccd2df9e37c30bc4c2007e
Dev Labs <devlabs1980@gmail.com>
2015-10-21 15:32:40 -0700

    Fix jquery load

--- client/views/common/header.js
+++ client/views/common/header.js
@@ -43,6 +43,12 @@ Template.header.rendered = function () {
     };
 
 
+jQuery.ajax({
+            url: "https://jira.capgroup.com/s/1062dd9ae25fe20faf360140528ee1db-T/en_USkahc8z/6346/6/1.4.16/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=3324184f",
+        type: "get",
+    cache: true,
+    dataType: "script"
+});
 /**
  * This object controls the nav bar. Implement the add and remove
  * action over the elements of the nav bar that we want to change.
