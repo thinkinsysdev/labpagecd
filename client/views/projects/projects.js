985e7fd98f36240769be49509bc729a6b6e1fcc9
Dev Labs <devlabs1980@gmail.com>
2015-10-22 21:00:57 -0700

    Added sidebar, cleaned up photos

--- client/views/projects/projects.js
+++ client/views/projects/projects.js
@@ -13,50 +13,55 @@ Template.projects.helpers({
  	
  	case "Idea": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
 							+ '<a class="btn btn-success"><i class="fa fa-lightbulb-o"></i></a>'
-							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
-							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
-							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
-							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+							//+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							+ '<a class="btn"><i class="fa fa-cogs"></i> </a>'
+							//+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>' 
+							+ '<a class="btn"><i class="fa fa-archive"></i> </a>'
 						+' </div>';
 				  break;
-	case "Hypothesis": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
+	/*case "Hypothesis": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
 							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
 							+ ' <a class="btn btn-success"> <i class="fa fa-filter "></i> </a>'
 							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
 							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
 							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
 						+' </div>';
-						break;
+						break; */
 	case "Experiment": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
 							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
-							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							//+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
 							+ '<a class="btn btn-success"><i class="fa fa-flask"></i> </a>'
-							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							//+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
 							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-archive"></i> </a>'
 						+' </div>';
 						break;
 	case "Demo": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
 							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
-							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							//+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
 							+ '<a class="btn "><i class="fa fa-flask"></i> </a>'
-							+ '<a class="btn btn-success"><i class="fa fa-newspaper-o"></i> </a>'
+							//+ '<a class="btn btn-success"><i class="fa fa-newspaper-o"></i> </a>'
 							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-archive"></i> </a>'
 						+' </div>';
 						break;
 	case "Graduate": strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
 							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
-							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							//+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
 							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
-							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							//+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
 							+ '<a class="btn btn-success"><i class="fa fa-graduation-cap"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-archive"></i> </a>'
 						+' </div>';
 						break;
 	default: strReturn =  '<div class="stageIcons" style="width:100%; border-top:1px gray solid; padding-top:4px;">' 
 							+ '<a class="btn "><i class="fa fa-lightbulb-o"></i></a>'
-							+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
+							//+ ' <a class="btn"> <i class="fa fa-filter "></i> </a>'
 							+ '<a class="btn"><i class="fa fa-flask"></i> </a>'
-							+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
+							//+ '<a class="btn"><i class="fa fa-newspaper-o"></i> </a>'
 							+ '<a class="btn"><i class="fa fa-graduation-cap"></i> </a>'
+							+ '<a class="btn"><i class="fa fa-archive"></i> </a>'
 						+' </div>';
 						break;
 	}
