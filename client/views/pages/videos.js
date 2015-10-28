9bad08550e2da171a0a409c87f2b89dcf136bd30
Dev Labs <devlabs1980@gmail.com>
2015-10-27 22:58:54 -0700

    Added project pictures, videos and a bunch other fixes

--- client/views/pages/videos.js
+++ client/views/pages/videos.js
@@ -1,7 +1,28 @@
 Template.videos.events({
-  'click #AppianModal': function(e) {
+  'click #AppianModalLink': function(e) {
     e.preventDefault();
     
-    $('#videosModal').modal('show');
+    $('#AppianModal').modal('show');
+  },
+   'click #CEPModalLink': function(e) {
+    e.preventDefault();
+    
+    $('#CEPModal').modal('show');
+  }, 
+  'click #DelphixModalLink': function(e) {
+    e.preventDefault();
+    
+    $('#DelphixModal').modal('show');
+  },
+   'click #LabRebuildModalLink': function(e) {
+    e.preventDefault();
+    
+    $('#LabRebuildModal').modal('show');
+  },
+  'click #DevEnablementModalLink': function(e) {
+    e.preventDefault();
+    
+    $('#DevEnablementModal').modal('show');
   }
+
 });
