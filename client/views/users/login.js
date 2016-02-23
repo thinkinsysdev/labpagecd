9d1ae66e40608719f77be4e9334eedd537ec2c37
Dev Labs <devlabs1980@gmail.com>
2016-02-23 07:13:12 -0800

    Cleaned up login, ideas and submit Idea page... need more work on styling and views

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -16,3 +16,34 @@ Template.login.helpers({
     }
 
 })
+
+Template.login.events({
+
+    'submit #loginForm' : function(e, t){
+      e.preventDefault();
+      // retrieve the input field values
+      var username = t.find('#loginUserName').value
+        , password = t.find('#loginPassword').value;
+
+        // Trim and validate your fields here.... 
+
+        // If validation passes, supply the appropriate fields to the
+        // Meteor.loginWithPassword() function.
+        Meteor.loginWithPassword(username, password, function(err){
+        if (err)
+        {
+          // The user might not have been found, or their passwword
+          // could be incorrect. Inform the user that their
+          // login attempt has failed. 
+          console.log('Login error triggered');
+          FlashMessages.sendError('Your request cannot be completed. Please contact TMB (x51643).');
+        }
+
+        else
+          // The user has been logged in.
+         Router.go('/adminPage');
+
+      });
+         return false; 
+      }
+  });
